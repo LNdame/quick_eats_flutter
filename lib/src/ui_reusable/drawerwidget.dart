f1c078e722c5a980646e159965f67cf4525fcb9d
LNdame <ls20045@gmail.com>
2019-09-15 21:12:24 +0200

    master seed

---
+++ lib/src/ui_reusable/drawerwidget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class UIDrawer extends StatefulWidget {
+  @override
+  _UIDrawerState createState() {
+    return new _UIDrawerState();
+  }
+}
+
+class _UIDrawerState extends State<UIDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Drawer(
+        child: ListView(
+      children: <Widget>[
+        UserAccountsDrawerHeader(
+          accountEmail: Text("Loic@email.com"),
+          accountName: Text("Loic Dev"),
+          currentAccountPicture: CircleAvatar(
+            backgroundImage:
+                NetworkImage('https://picsum.photos/200/300/?blur=2'),
+            backgroundColor: Colors.white,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.blueGrey,
+          ),
+        ),
+        ListTile(
+          title: Text("Home"),
+          trailing: Icon(Icons.home),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text("Orders"),
+          trailing: Icon(Icons.toll),
+          onTap: () {},
+        )
+      ],
+    ));
+  }
+}
---
+++ lib/src/views/sign_in_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget{
+  @override
+  _SignInPageState createState() =>new _SignInPageState();
+
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text("Sign In"),
+    );
+  }
+}
